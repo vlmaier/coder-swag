50f88ff7ca8474e5aae90220ec5975c99bdd9209
Vladas Maier <vladas.mr@googlemail.com>
2019-08-15 16:34:31 +0200

    Replaced ListView with RecyclerView implementation

---
+++ app/src/main/java/org/vmaier/coderswag/adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,43 @@
+package org.vmaier.coderswag.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.vmaier.coderswag.R
+import org.vmaier.coderswag.model.Category
+
+class CategoryRecycleAdapter(val context : Context, val categories : List<Category>) :
+    RecyclerView.Adapter<CategoryRecycleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindCategory(categories[position], context)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val categoryImage = itemView.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView.findViewById<TextView>(R.id.categoryName)
+
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId = context.resources.getIdentifier(category.image,
+                "drawable", context.packageName)
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.title
+        }
+    }
+}
--- app/src/main/java/org/vmaier/coderswag/controller/MainActivity.kt
+++ app/src/main/java/org/vmaier/coderswag/controller/MainActivity.kt
@@ -2,21 +2,25 @@ package org.vmaier.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.vmaier.coderswag.R
-import org.vmaier.coderswag.adapters.CategoryAdapter
+import org.vmaier.coderswag.adapters.CategoryRecycleAdapter
 import org.vmaier.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
-
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
